90fb72c4c519228d0ddba35233e74a189942b163
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2015-05-05 01:20:00 +0200

    Update test_script

--- JeremyAnsel.ColorQuant/run-tests.cmd
+++ JeremyAnsel.ColorQuant/run-tests.cmd
@@ -4,9 +4,9 @@ setlocal
 if '%Configuration%' == '' if not '%1' == '' set Configuration=%1
 if '%Configuration%' == '' set Configuration=Debug
 
-if exist .\build\coverage rd /s /q .\build\coverage
-md .\build\coverage
+if exist "%~dp0\build\coverage" rd /s /q "%~dp0\build\coverage"
+md "%~dp0\build\coverage"
 
-.\packages\OpenCover.4.5.3723\OpenCover.Console.exe -register:user -output:.\build\coverage\results.xml -target:.\packages\xunit.runner.console.2.0.0\tools\xunit.console.exe "-targetargs:.\JeremyAnsel.ColorQuant.Tests\bin\%Configuration%\JeremyAnsel.ColorQuant.Tests.dll -noshadow" "-filter:+[JeremyAnsel.ColorQuant]* -[*.Tests]*" -hideskipped:File;Filter;Attribute
+"%~dp0\packages\OpenCover.4.5.3723\OpenCover.Console.exe" -register:user -output:"%~dp0\build\coverage\results.xml" -target:"%~dp0\packages\xunit.runner.console.2.0.0\tools\xunit.console.exe" -targetargs:"%~dp0\JeremyAnsel.ColorQuant.Tests\bin\%Configuration%\JeremyAnsel.ColorQuant.Tests.dll -noshadow" "-filter:+[JeremyAnsel.ColorQuant]* -[*.Tests]*" -hideskipped:File;Filter;Attribute
 
-.\packages\ReportGenerator.2.1.4.0\ReportGenerator.exe -reports:.\build\coverage\results.xml -targetdir:.\build\coverage -verbosity:Info
+"%~dp0\packages\ReportGenerator.2.1.4.0\ReportGenerator.exe" -reports:"%~dp0\build\coverage\results.xml" -targetdir:"%~dp0\build\coverage" -verbosity:Info
