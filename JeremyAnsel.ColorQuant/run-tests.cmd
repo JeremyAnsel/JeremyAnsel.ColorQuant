49a2a31bc1f4ce08c6615debd0ff5c7d7f62759c
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2015-09-27 16:48:31 +0200

    Update build dependencies

--- JeremyAnsel.ColorQuant/run-tests.cmd
+++ JeremyAnsel.ColorQuant/run-tests.cmd
@@ -9,7 +9,7 @@ if '%Configuration%' == '' set Configuration=Debug
 if exist build\coverage rd /s /q build\coverage
 md build\coverage
 
-packages\OpenCover.4.5.3723\OpenCover.Console.exe -register:user -output:build\coverage\results.xml -target:packages\xunit.runner.console.2.0.0\tools\xunit.console.exe -targetargs:"JeremyAnsel.ColorQuant.Tests\bin\%Configuration%\JeremyAnsel.ColorQuant.Tests.dll -noshadow" "-filter:+[JeremyAnsel.ColorQuant]* -[*.Tests]*" -hideskipped:File;Filter;Attribute -returntargetcode
+packages\OpenCover.4.6.166\tools\OpenCover.Console.exe -register:user -output:build\coverage\results.xml -target:packages\xunit.runner.console.2.0.0\tools\xunit.console.exe -targetargs:"JeremyAnsel.ColorQuant.Tests\bin\%Configuration%\JeremyAnsel.ColorQuant.Tests.dll -noshadow" "-filter:+[JeremyAnsel.ColorQuant]* -[*.Tests]*" -hideskipped:File;Filter;Attribute -returntargetcode
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-packages\ReportGenerator.2.1.4.0\ReportGenerator.exe -reports:build\coverage\results.xml -targetdir:build\coverage -verbosity:Info
+packages\ReportGenerator.2.3.2.0\tools\ReportGenerator.exe -reports:build\coverage\results.xml -reporttypes:Html;Badges -targetdir:build\coverage -verbosity:Info
