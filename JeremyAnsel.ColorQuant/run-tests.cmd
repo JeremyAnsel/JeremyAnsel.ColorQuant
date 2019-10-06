75ac116597af33f0844e8ebf2b129d00e5af1bae
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2019-10-06 18:55:22 +0200

    Update run-tests.cmd

--- JeremyAnsel.ColorQuant/run-tests.cmd
+++ JeremyAnsel.ColorQuant/run-tests.cmd
@@ -12,10 +12,10 @@ dotnet tool update dotnet-reportgenerator-globaltool --tool-path packages
 if exist bld\coverage rd /s /q bld\coverage
 md bld\coverage
 
-packages\coverlet "JeremyAnsel.ColorQuant.Tests\bin\%Configuration%\netcoreapp3.0\JeremyAnsel.ColorQuant.Tests.dll" --target "dotnet" --targetargs "test -f netcoreapp3.0 --no-build" --output "bld\coverage\results-netcoreapp3.0.xml" --format opencover --use-source-link
+packages\coverlet "JeremyAnsel.ColorQuant.Tests\bin\%Configuration%\netcoreapp3.0\JeremyAnsel.ColorQuant.Tests.dll" --target "dotnet" --targetargs "test -f netcoreapp3.0 --no-build" --output "bld\coverage\results-netcoreapp3.0.xml" --format opencover
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-packages\coverlet "JeremyAnsel.ColorQuant.Tests\bin\%Configuration%\net452\JeremyAnsel.ColorQuant.Tests.dll" --target "dotnet" --targetargs "test -f net452 --no-build" --output "bld\coverage\results-net452.xml" --format opencover --use-source-link
+packages\coverlet "JeremyAnsel.ColorQuant.Tests\bin\%Configuration%\net452\JeremyAnsel.ColorQuant.Tests.dll" --target "dotnet" --targetargs "test -f net452 --no-build" --output "bld\coverage\results-net452.xml" --format opencover
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 packages\reportgenerator -reports:"bld\coverage\results-netcoreapp3.0.xml;bld\coverage\results-net452.xml" -reporttypes:Html;Badges -targetdir:bld\coverage -verbosity:Info
